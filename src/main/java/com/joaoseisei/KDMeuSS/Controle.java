3c082a7dbd33514809e181ac15d13e9a0b3fa7e1
joaoseisei <109870486+joaoseisei@users.noreply.github.com>
2023-08-07 22:09:41 -0300

    adicionando javadoc

--- src/main/java/com/joaoseisei/KDMeuSS/Controle.java
+++ src/main/java/com/joaoseisei/KDMeuSS/Controle.java
@@ -18,21 +18,42 @@ public class Controle {
     private ArrayList<Materia> preferencias = new ArrayList<>();
     Dados dados = new Dados();
     Memoria memoria = new Memoria();
+
+    /**
+     * Envia uma lista de grades possiveis com as preferências.
+     * @return OK
+     */
     @GetMapping
     public ResponseEntity<List<Grade>> getGrades(){
         memoria.setMaterias(dados.getMaterias());
         GerenciadorGrades gerenciadorGrades = new GerenciadorGrades(memoria);
         return ResponseEntity.ok(gerenciadorGrades.gerarGrades(preferencias));
     }
+
+    /**
+     * Processa a lista de preferências do front.
+     * @param materias Lista de matérias recebidas.
+     * @return OK
+     */
     @PostMapping("/preferencias")
     public ResponseEntity<String> processarJson(@RequestBody ArrayList<Materia> materias){
         this.preferencias = materias;
         return ResponseEntity.ok("Preferencias recebidas");
     }
+
+    /**
+     * Envia uma lista de Materias para /listaProfessores.
+     * @return Lista de strings com nome dos professores.
+     */
     @GetMapping("/listaMaterias")
     public ResponseEntity<Set<String>> getNomeMaterias(){
         return ResponseEntity.ok(dados.getMaterias().stream().map(Materia::getNome).collect(Collectors.toSet()));
     }
+
+    /**
+     * Envia uma lista de professores para /listaProfessores
+     * @return Lista de strings com nome dos professores.
+     */
     @GetMapping("/listaProfessores")
     public ResponseEntity<Set<String>> getNomeProfessores(){
         return ResponseEntity.ok(dados.getMaterias().stream().map(Materia::getProfessor).collect(Collectors.toSet()));
